605ed6b0e47f175a1de5690a23a465379a06c6a6
Saptarshi <chakrabortysaptarshi066@gmail.com>
2026-07-23 11:06:30 +0530

    chore: remove account-service module

--- account-service/src/main/java/com/bankingecommerce/accountservice/exception/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package com.bankingecommerce.accountservice.exception;
-
-public class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
--- account-service/src/main/java/com/bankingecommerce/accountservice/model/AccountStatus.java
+++
@@ -1,7 +0,0 @@
-package com.bankingecommerce.accountservice.model;
-
-public enum AccountStatus {
-    ACTIVE,
-    FROZEN,
-    CLOSED
-}
